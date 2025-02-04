9a8d7d35e57f701a60ef04dea402092e5e3bec93
kushpatel7764 <94019918+kushpatel7764@users.noreply.github.com>
2025-02-04 10:00:43 -0500

    More linting updates

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -11,8 +11,6 @@ from api_secrets import gemini_api_key
 
 # Function to get a random job listing from the JSON file
 def get_random_json_object():
-
-
     with open('rapid_jobs2.json', 'r') as f:
         json_objects = []
         for line in f:
@@ -27,6 +25,7 @@ def get_random_json_object():
 # Get the API key from the api_secrets file to access Google's Gemini AI model
 key = gemini_api_key
 
+
 # Using the random_json_object(), get a random job and then extract the job description from the job.
 job = get_random_json_object()
 job_description = job["description"]
@@ -36,12 +35,14 @@ personal_info = ("My name is Kush Patel. I am a computer science major studying 
                  " I am driven by a desire to innovate and problem solve. I am graduating from the BSU on May, 2025. "
                  "Some important courses I have completed at BSU are Web Application Development, Computer Networks, "
                  "Software Engineering, Cloud Computing, Introduction Database systems, Introduction to A.I., and Unix/Linux System Admin."  
-                 "I was awarded Dr. Linda Wilkens and Dr. Glenn Pavlicek Scholarship at BSU in recognition of my academic achievements such as a 4.0 major GPA."
-                 " I am from East Greenwich, Road Island but I currently live in Fairhaven, Massachusetts. Some programing languages that I am proficient at are Python, Java, Swift, and JavaScript. "
-                 "I also MySQL from my database system course. Currently, I am developing a bank statement processing application that efficiently converts bank statements into Excel spreadsheets. "
-                 "With a positive attitude and a relentless motivation to learn, I'm eager to take on new challenges and expand my expertise. Additionally, along with my current project, I am also doing "
-                 "a research internship at Bridgewater State University in which I am developing a program that can recognize a table in an image and convert it to an excel table. Some tools that I know"
-                 " are Git, JetBrain IDEs, Xcode, and Vscode. My hobbies are playing basketball, cricket, and programing.")
+                 "I was awarded Dr. Linda Wilkens and Dr. Glenn Pavlicek Scholarship at BSU in recognition of my academic achievements such"
+                 " as a 4.0 major GPA. I am from East Greenwich, Road Island but I currently live in Fairhaven, Massachusetts. Some programing "
+                 "languages that I am proficient at are Python, Java, Swift, and JavaScript. I also MySQL from my database system course. Currently, "
+                 "I am developing a bank statement processing application that efficiently converts bank statements into Excel spreadsheets. With a "
+                 "positive attitude and a relentless motivation to learn, I'm eager to take on new challenges and expand my expertise. Additionally, "
+                 "along with my current project, I am also doing a research internship at Bridgewater State University in which I am developing a program "
+                 "that can recognize a table in an image and convert it to an excel table. Some tools that I know are Git, JetBrain IDEs, Xcode, and Vscode. "
+                 "My hobbies are playing basketball, cricket, and programing.")
 
 # Prepare the API request payload for Google's Gemini AI model
 url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={key}"
